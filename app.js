9b76e19c55061ea3d1d132f35dee47b4ee9aaee4
Eugene-im <em.92@mail.ru>
2020-07-22 18:31:54 +0300

    version 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fsx = require("fs-extra");
 const fs = require("fs");
 const path = require('path');
 const cp = require('child_process');
@@ -10,46 +11,53 @@ streamsUrl.zal1 = 'rtsp://admin:liana2017@10.12.19.8:554/Streaming/Channels/1802
 streamsUrl.zal2 = 'rtsp://admin:liana2017@10.12.19.8:554/Streaming/Channels/1702';
 streamsUrl.zal3 = 'rtsp://admin:liana2017@10.12.19.8:554/Streaming/Channels/1602';
 streamsUrl.zal4 = 'rtsp://admin:liana2017@10.12.19.8:554/Streaming/Channels/1502';
-const cmd = 'ffmpeg -i ' + streamsUrl.zal1 + ' -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 video/output.m3u8'
-var asd;
+
+var asd ={};
+var zal;
 
 app.listen(8000);
 
 
-app.get('/1', function (req, res) {
-  if (!fs.existsSync(directory)) {
-    fs.mkdirSync(directory);
-  } else {
-    deleteFolder();
-  }
-  res.send(req.url + " hello " + req.headers["user-agent"]);
-  asd = cp.exec(cmd, function (err, stdout, stderr) {
-    if (err) {
-      console.log(err)
+app.get('/start:id', function (req, res) {
+  zal = `zal${req.params.id}`;
+  var cmd = `ffmpeg -i ${streamsUrl[zal]} -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 ${directory}/${zal}/index.m3u8`;
+
+  if (req.params.id < 5) {
+    if (!fs.existsSync(`${directory}/${zal}`)) {
+      fsx.mkdirs(`${directory}/${zal}`);
+    } else {
+      deleteFolder();
     }
-  })
+    res.send(`${zal} start streaming`);
+    asd[zal] = cp.exec(cmd, function (err, stdout, stderr) {
+      if (err) {
+        console.log(err)
+      }
+    })
+  } else {
+    res.send(`check zal, zal ${zal} is exist`);
+  }
 });
 
 
-app.get('/2', function (req, res) {
-  res.send(req.url + " bye " + req.headers["user-agent"]);
-  asd.stdin.write('q');
-  asd.on('exit', function () {
-    asd = null;
-    deleteFolder();
-  })
+app.get('/stop:id', function (req, res) {
+  zal = `zal${req.params.id}`;
+  if (req.params.id < 5) {
+    res.send(`${zal} stop streaming`);
+    asd[zal].stdin.write('q');
+    asd[zal].on('exit', function () {
+      asd[zal] = null;
+      deleteFolder();
+    })
+  } else {
+    res.send(`check zal, zal ${zal} is exist`);
+  }
 })
 
 function deleteFolder() {
-  if (fs.existsSync(directory)) {
-    fs.readdirSync(directory).forEach((file, index) => {
-      const curPath = path.join(directory, file);
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(directory);
+  if (!fsx.ensureDirSync(`${directory}/${zal}`)) {
+    fsx.remove(`${directory}/${zal}`, err => {
+      console.error(err)
+    })
   }
 }
